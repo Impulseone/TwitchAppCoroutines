21629b40a8ea0fb0b310cca3873ce14f2f7c2e51
Крайнюков Сергей <skrajnjukov@rctahograf.ru>
2021-11-18 08:25:25 +0300

    followers repo refactor

--- app/src/main/java/com/mycorp/twitchapprxjava/repository/FollowersRepositoryImplementation.kt
+++ app/src/main/java/com/mycorp/twitchapprxjava/repository/FollowersRepositoryImplementation.kt
@@ -12,18 +12,19 @@ class FollowersRepositoryImplementation(
     private val followersController: FollowersController,
     private val followersStorage: FollowersStorage
 ) : FollowersRepository {
-    override fun fetchFollowers(id: String): Single<List<FollowerInfo>> =
-        followersController.getGameItemDataFromNetwork(id).map {
-            val followers = it.follows?.map { followerDto -> FollowerInfo.fromFollowerDto(followerDto!!) }
-            if (followers != null) {
-                insertFollowers(followers, id)
+    override fun fetchFollowers(id: String): Single<List<FollowerInfo>> {
+        return followersController.getGameItemDataFromNetwork(id).map {
+            it.follows!!.map { followerDto -> FollowerInfo.fromFollowerDto(followerDto!!) }
+        }.doOnSuccess {
+            if (it != null) {
+                followersStorage.insertFollowersData(it, id)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({},{})
+                    .subscribe({}, {})
                     .dispose()
             }
-            return@map followers
         }
+    }
 
     override fun getFollowersByIds(followerIds: List<String>) =
         followersStorage.getFollowersByIds(followerIds)
@@ -31,12 +32,5 @@ class FollowersRepositoryImplementation(
 
     override fun getFollowersIdByGameId(gameId: String): Single<List<String>> {
         return followersStorage.getFollowersIdByGameId(gameId)
-    }
-
-    private fun insertFollowers(
-        followersList: List<FollowerInfo>,
-        gameId: String
-    ): Completable {
-        return followersStorage.insertFollowersData(followersList, gameId)
     }
 }
