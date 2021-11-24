93eec9477503b4ae9b81d47f6ae3f7a43605fc10
Sergei Krainyukov <sk-new@mail.ru>
2021-11-24 11:53:08 +0300

    model module completed

--- app/src/main/java/com/mycorp/twitchapprxjava/App.kt
+++ app/src/main/java/com/mycorp/twitchapprxjava/App.kt
@@ -5,11 +5,13 @@ import com.mycorp.twitchapprxjava.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
 
 class App : Application(){
     override fun onCreate() {
         super.onCreate()
         startKoin{
+//            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidLogger()
             androidContext(this@App)
             modules(listOf(
