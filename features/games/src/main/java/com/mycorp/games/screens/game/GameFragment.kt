3b183d213b1f50d31b2926c6f4fbac3c67a8f97c
Sergei Krainyukov <sk-new@mail.ru>
2021-12-10 13:39:29 +0300

    add lidecycle fragment extension

--- features/games/src/main/java/com/mycorp/games/screens/game/GameFragment.kt
+++ features/games/src/main/java/com/mycorp/games/screens/game/GameFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
+import com.mycorp.common.extensions.collectFlow
 import com.mycorp.common.fragment.BaseFragment
 import com.mycorp.games.R
 import com.mycorp.games.databinding.FragmentGameBinding
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class GameFragment :
@@ -47,33 +43,37 @@ class GameFragment :
 
     override fun bindVm() {
         super.bindVm()
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                with(binding) {
-                    with(viewModel) {
-                        launch {
-                            gameDataInfoFlow.collectLatest {
-                                progressIndicator.isVisible = false
-                                gameName.text = it.gameData.name
-                                Glide.with(requireContext()).load(it.gameData.logoUrl).into(image)
-                                followersCount.text =
-                                    getString(
-                                        R.string.scr_game_layout_followers_tv,
-                                        it.followers.size.toString()
-                                    )
-                            }
-                        }
-                        launch {
-                            isFavoriteFlow.collectLatest {
-                                like.setImageDrawable(
-                                    ContextCompat.getDrawable(
-                                        requireContext(),
-                                        if (it) R.drawable.like_filled_icon else R.drawable.like_outlined_icon
-                                    )
-                                )
-                            }
-                        }
-                    }
+        bindGameDataInfoFlow()
+        bindIsFavoriteFlow()
+    }
+
+    private fun bindGameDataInfoFlow() {
+        with(binding) {
+            with(viewModel) {
+                collectFlow(gameDataInfoFlow) {
+                    progressIndicator.isVisible = false
+                    gameName.text = it.gameData.name
+                    Glide.with(requireContext()).load(it.gameData.logoUrl).into(image)
+                    followersCount.text =
+                        getString(
+                            R.string.scr_game_layout_followers_tv,
+                            it.followers.size.toString()
+                        )
+                }
+            }
+        }
+    }
+
+    private fun bindIsFavoriteFlow() {
+        with(binding) {
+            with(viewModel) {
+                collectFlow(isFavoriteFlow) {
+                    like.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            if (it) R.drawable.like_filled_icon else R.drawable.like_outlined_icon
+                        )
+                    )
                 }
             }
         }
